4db8fad54302b24be2eb091a6c9876f544aeaead
AJEESH <astroboy932000@gmail.com>
2025-01-27 23:02:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
  var radius = 240; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
+var imgWidth = 150; // width of images (unit: px)
+var imgHeight = 190; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
 var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
